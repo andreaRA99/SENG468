9c6377fbd38160efb677495a0b3780ff39a90626
Ryan O’Hara <ryan@ohara.me>
2023-02-23 21:01:31 -0800

    gofmt

--- transaction-server/mongo_reader.go
+++ transaction-server/mongo_reader.go
@@ -3,29 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson"
+	"log"
 )
 
-
-func readOne(){
-
+func readOne() {
 
 }
 
-func readMany(){
+func readMany() {
 
 }
 
 func read(collection_ string, p string) {
 	ctx := context.TODO()
-  uri := "mongodb+srv://daytrading.bpyesvi.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=/Users/mateomoody/Desktop/X509-cert-3374770886339045150.cer"
-  clientOptions := options.Client().ApplyURI(uri)
+	uri := "mongodb+srv://daytrading.bpyesvi.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=/Users/mateomoody/Desktop/X509-cert-3374770886339045150.cer"
+	clientOptions := options.Client().ApplyURI(uri)
 
 	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer client.Disconnect(ctx)
 
 	pattern := bson.D{{"user_id", p}}
@@ -36,15 +36,17 @@ func read(collection_ string, p string) {
 
 	if err != nil {
 		panic(err)
-  	}
-  	var results []bson.D
+	}
+	var results []bson.D
 
-  	if err = cursor.All(context.TODO(), &results); err != nil {
+	if err = cursor.All(context.TODO(), &results); err != nil {
 		panic(err)
-  	}
+	}
 	fmt.Println("RESULTS")
 	fmt.Println(results)
 	docCount, err := collection.CountDocuments(ctx, bson.D{})
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(docCount)
 }
--- transaction-server/mongo_writer.go
+++ transaction-server/mongo_writer.go
@@ -3,26 +3,30 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson"
+	"log"
 )
 
 func insert(collection_ string, data bson.D) {
 	ctx := context.TODO()
-  uri := "mongodb+srv://daytrading.bpyesvi.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=/Users/mateomoody/Desktop/X509-cert-3374770886339045150.cer"
-  clientOptions := options.Client().ApplyURI(uri)
+	uri := "mongodb+srv://daytrading.bpyesvi.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=/Users/mateomoody/Desktop/X509-cert-3374770886339045150.cer"
+	clientOptions := options.Client().ApplyURI(uri)
 
 	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer client.Disconnect(ctx)
 
 	collection := client.Database("myFirstDatabase").Collection("TEST")
-	result, err := collection.InsertOne(ctx, bson.D{{"user_id","10"}, {"encrypted_key","10"}})
+	result, err := collection.InsertOne(ctx, bson.D{{"user_id", "10"}, {"encrypted_key", "10"}})
 	fmt.Println(result)
 	fmt.Println(err)
 	docCount, err := collection.CountDocuments(ctx, bson.D{})
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(docCount)
 }
