6caddd713072754433fb8fe91e35767c5c2b66b6
Dennis Arimi <dennisarimi@uvic.ca>
2023-03-03 11:07:13 -0800

    Renaming function that gets quote price

--- transaction-server/transaction-server.go
+++ transaction-server/transaction-server.go
@@ -50,7 +50,7 @@ var orders = []order{}
 // main
 func main() {
 	sym, username := getParams()
-	quotePrice, timestamp, cryptKey := connectToServer(sym, username)
+	quotePrice, timestamp, cryptKey := getQuote(sym, username)
 
 	fmt.Println("qoute price = ", quotePrice, "\nsym = ", sym, "\nusername = ", username, "\ntimestamp = ", timestamp, "\ncrypt key = ", cryptKey)
 
@@ -194,7 +194,7 @@ func getParams() (string, string) {
 	return sym, usrnme
 }
 
-func connectToServer(sym string, username string) (string, string, string) {
+func getQuote(sym string, username string) (string, string, string) {
 	//make connection to server
 	strEcho := sym + " " + username + "\n"
 	servAddr := "quoteserve.seng.uvic.ca:4444"
