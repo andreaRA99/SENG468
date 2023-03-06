7a123e9c44221b74d21c5fb33037244fa0e61c12
Ryan O’Hara <ryan@ohara.me>
2023-03-06 23:59:48 +0000

    go fmt

--- transaction-server/mongo_util.go
+++ transaction-server/mongo_util.go
@@ -5,59 +5,55 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-
-
-func mongo_read_bsonA(v bson.A) ([]holding){
-	// Rather than Unmarshalling, which is best practice, this is a hack workaround since 
+func mongo_read_bsonA(v bson.A) []holding {
+	// Rather than Unmarshalling, which is best practice, this is a hack workaround since
 	// I couldnt get the Unmarshalling to work.
-
 
 	// Hard coded to only work for reading for fields in account_holdings[{}]
 
 	// bson.A is an array of bson.D, which contains bson.E, which is key value pairs.
-	// loop through A to find each bson.D that corresponds to a document per holding, loop through that bson.D 
+	// loop through A to find each bson.D that corresponds to a document per holding, loop through that bson.D
 	// to extract the value
 
-	var e []holding		
+	var e []holding
 
-
-	for _, s := range v{
+	for _, s := range v {
 		fmt.Println(s)
 		var temp holding
 
-		switch c := s.(type){
-			case bson.D:
+		switch c := s.(type) {
+		case bson.D:
 			{
 				for _, kv_pair := range c {
-			
-					switch d := kv_pair.Value.(type){
-						case string:
+
+					switch d := kv_pair.Value.(type) {
+					case string:
 						{
 							temp.symbol = d
 						}
-						case int32:
+					case int32:
 						{
-							fmt.Printf("Should never be this\n\n",d)
-							fmt.Printf("%d\n\n",d)
+							fmt.Printf("Should never be this\n\n", d)
+							fmt.Printf("%d\n\n", d)
 
-						}			
-						case float64:
+						}
+					case float64:
 						{
 							if kv_pair.Key == "quantity" {
 								temp.quantity = d
-							} 	else // Value is pps
+							} else // Value is pps
 							{
 								temp.pps = d
 							}
 
 						}
 					}
-				
+
 				}
 			}
 		}
 		e = append(e, temp)
 	}
-	
+
 	return e
 }
--- transaction-server/mongo_writer.go
+++ transaction-server/mongo_writer.go
@@ -1,27 +1,24 @@
-
 package main
 
 import (
 	"context"
-	"log"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson"
+	"log"
 )
 
-
-
-func updateOne(collection_ string, who bson.D, with bson.D, _type string) string{
+func updateOne(collection_ string, who bson.D, with bson.D, _type string) string {
 
 	update := bson.D{{_type, with}}
 
 	ctx := context.TODO()
-   uri := "mongodb+srv://daytrading.bpyesvi.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=/Users/mateomoody/Desktop/X509-cert-3374770886339045150.cer"
-   clientOptions := options.Client().ApplyURI(uri)
+	uri := "mongodb+srv://daytrading.bpyesvi.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=/Users/mateomoody/Desktop/X509-cert-3374770886339045150.cer"
+	clientOptions := options.Client().ApplyURI(uri)
 
 	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil { 
-		log.Fatal(err) 
+	if err != nil {
+		log.Fatal(err)
 		panic(err)
 		return "Failed to Update Value"
 	}
@@ -29,29 +26,30 @@ func updateOne(collection_ string, who bson.D, with bson.D, _type string) string
 
 	collection := client.Database("daytrading").Collection(collection_)
 	_, err = collection.UpdateOne(ctx, who, update)
-	if err != nil { 
-		log.Fatal(err) 
+	if err != nil {
+		log.Fatal(err)
 		panic(err)
 		return "Failed to Update Value"
 	}
 
 	return "ok"
 
-
 }
 func insert(collection_ string, data bson.D) string {
 	ctx := context.TODO()
-  uri := "mongodb+srv://daytrading.bpyesvi.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=/Users/mateomoody/Desktop/X509-cert-3374770886339045150.cer"
-  clientOptions := options.Client().ApplyURI(uri)
+	uri := "mongodb+srv://daytrading.bpyesvi.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=/Users/mateomoody/Desktop/X509-cert-3374770886339045150.cer"
+	clientOptions := options.Client().ApplyURI(uri)
 
 	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer client.Disconnect(ctx)
 
 	collection := client.Database("daytrading").Collection(collection_)
 	_, err = collection.InsertOne(ctx, data)
-	if err != nil { 
-		log.Fatal(err) 
+	if err != nil {
+		log.Fatal(err)
 		return "Failed to Insert Value"
 	}
 
