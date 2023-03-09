0bb2c076fac5d3860a47daee6557e466ad755567
Radu Ionescu <raduionescu@w134-87-179-107.wireless.uvic.ca>
2023-03-09 14:48:20 -0800

    Adding non-functioning CLI

---
+++ transaction-server/cli.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+	"net/http"	
+	"os"
+	
+)
+
+
+func CLI() {
+	
+	requestURL := fmt.Sprintf("http://localhost:8080/health")
+	req, err := http.NewRequest("Get", requestURL, nil)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+		os.Exit(1)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("client: could not read response body: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("client: response body: %s\n", resBody)
+}
+	
