a63e6c9bfb0b40cf2d2a298a6e2fbeff08ce1b93
andreaRA99 <andrea_rmz.alcala@hotmail.com>
2023-03-20 16:12:33 -0700

    Removing Duplicated functions

--- transaction-server/transaction-server.go
+++ transaction-server/transaction-server.go
@@ -166,6 +166,19 @@ func addAccount(id string) account {
 	return newAccount
 }
 
+// ADD ACCOUNT IS NEVER CALLED BY A REQUEST DIRECTLY
+// func addAccount(c *gin.Context) {
+// 	var newAccount account
+
+// 	// Call BindJSON to bind the received JSON to newAccount.
+// 	if err := c.BindJSON(&newAccount); err != nil {
+// 		return
+// 	}
+// 	// Add the new account to the slice.
+// 	accounts = append(accounts, newAccount)
+// 	c.IndentedJSON(http.StatusCreated, newAccount)
+// }
+
 // THIS CODE MIGHT BE USEFUL IF WE DO SIGN UP FEATURE
 // func addAccount(c *gin.Context) {
 // 	var newAccount account
@@ -198,10 +211,37 @@ func addBalance(c *gin.Context) {
 
 }
 
+// DELETE WHEN addBalance is done
+// func addBalance(c *gin.Context) {
+// 	//id := c.Param("id")
+
+// 	var addingAmount balanceDif
+// 	//fmt.Println(addingAmount)
+
+// 	// Call BindJSON to bind recieved json to newBalance type
+// 	if err := c.BindJSON(&addingAmount); err != nil {
+// 		return
+// 	}
+
+// 	fmt.Println(addingAmount.ID, addingAmount.Adding)
+
+// 	for index, i := range accounts {
+// 		if i.ID == addingAmount.ID {
+// 			accounts[index].Balance = i.Balance + addingAmount.Adding
+
+// 			fmt.Println(i.Balance)
+
+// 			// Change this
+// 			c.IndentedJSON(http.StatusCreated, accounts)
+// 		}
+// 	}
+// }
+
 func getQuote(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "INCOMPLETE")
 
 }
+
 func getQuoteLocal(sym string) float64 {
 	// WILL BE DELETED LATER
 	// JUST SO THAT THERE IS A RETURN VALUE
@@ -428,273 +468,5 @@ func healthcheck(c *gin.Context) {
 	} else {
 		c.String(http.StatusInternalServerError, "mongo read unavailable")
 		log.Println(err)
-	}
-}
-
-func getQuote(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "INCOMPLETE")
-
-}
-func getQuoteLocal(sym string) float64 {
-	// WILL BE DELETED LATER
-	// JUST SO THAT THERE IS A RETURN VALUE
-	return 1
-}
-
-func getQuoteTEMP(sym string, username string) (float64, string, string) {
-	//TEMPORARY NAME BECAUSE IT INTERFERS WITH GET QUOTE HTTP METHOD
-	//make connection to server
-	strEcho := sym + " " + username + "\n"
-	servAddr := "quoteserve.seng.uvic.ca:4444"
-
-	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
-	if err != nil {
-		fmt.Println("\nResolveTCPAddr error: ", err)
-		panic(err)
-	}
-
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		fmt.Println("\nDialTCP error: ", err)
-		panic(err)
-	}
-
-	//write to server SYM being requested and user
-	_, err = conn.Write([]byte(strEcho))
-	if err != nil {
-		fmt.Println("\nWrite error: ", err)
-		panic(err)
-	}
-
-	//reading from server
-	_reply := make([]byte, 1024)
-
-	_, err = conn.Read(_reply)
-	if err != nil {
-		fmt.Println("\nRead error: ", err)
-		panic(err)
-	}
-
-	//parsing reply from server
-	reply := strings.Split(strings.ReplaceAll(string(_reply), "\n", ""), ",")
-	quotePrice, err := strconv.ParseFloat(reply[0], 64)
-	if err != nil {
-		panic(err)
-	}
-	timestamp := reply[3]
-	cryptKey := reply[4]
-
-	conn.Close()
-
-	return quotePrice, timestamp, cryptKey
-}
-
-func buyStock(c *gin.Context) {
-	id := c.Param("id")
-	stock := c.Param("stock")
-	quantity, err := strconv.ParseFloat(c.Param("quantity"), 64)
-	pps := getQuoteLocal(stock)
-
-	if err != nil {
-		panic("ERR")
-	}
-
-	r := readField("users", bson.D{{"user_id", id}}, bson.D{{"cash_balance", 1}})
-
-	n := bson.D{{"none", "none"}}
-
-	if reflect.DeepEqual(r, n) {
-		panic("ERROR")
-	}
-	cost := pps * quantity
-
-	fmt.Printf("\nTYPE = %T\n", r[0][1].Value)
-	fmt.Printf("\nTYPE = %T\n", cost)
-
-	// Check if user has enough balance
-	switch v := r[0][1].Value.(type) {
-	case float64:
-		{
-			fmt.Println("FLOATING")
-			if v > cost {
-				r := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"cash_balance", v - cost}}, "$set")
-				i := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", bson.D{{"symbol", stock}, {"quantity", quantity}, {"pps", pps}}}}, "$push")
-				if i != "ok" {
-					panic("PUSH ERROR")
-				}
-				if r != "ok" {
-					panic(r)
-				}
-				//c.IndentedJSON(http.StatusBadRequest, accounts[index])
-				return
-			}
-		}
-	case int64:
-		{
-			a := float64(v)
-			fmt.Println(a)
-			fmt.Println(cost)
-			if a > cost {
-				fmt.Println("YES")
-				r := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"cash_balance", a - cost}}, "$set")
-				i := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", bson.D{{"symbol", stock}, {"quantity", quantity}, {"pps", pps}}}}, "$push")
-				if i != "ok" {
-					panic("PUSH ERROR")
-				}
-				if r != "ok" {
-					panic(r)
-				}
-				//c.IndentedJSON(http.StatusBadRequest, accounts[index])
-				return
-			}
-		}
-	case int32:
-		{
-			a := float64(v)
-			fmt.Println(a)
-			fmt.Println(cost)
-			if a > cost {
-				fmt.Println("INT32")
-				r := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"cash_balance", a - cost}}, "$set")
-				i := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", bson.D{{"symbol", stock}, {"quantity", quantity}, {"pps", pps}}}}, "$push")
-				if i != "ok" {
-					panic("PUSH ERROR")
-				}
-				if r != "ok" {
-					panic(r)
-				}
-				//c.IndentedJSON(http.StatusBadRequest, accounts[index])
-				return
-			}
-		}
-	}
-
-	// User has enough balance, proceed creating order
-	//buy_id := len(orders) + 1
-	//newOrder.Buy_id = buy_id
-	//orders = append(orders, newOrder)
-	//return
-	//c.IndentedJSON(http.StatusOK, newOrder)
-}
-
-func sellStock(c *gin.Context) {
-	id := c.Param("id")
-	stock := c.Param("stock")
-	quantity, err := strconv.ParseFloat(c.Param("quantity"), 64)
-	pps := getQuoteLocal(stock)
-
-	if err != nil {
-		panic("ERR")
-	}
-	y := rawreadField("users", bson.D{{"user_id", id}}, bson.D{{"cash_balance", 1}})
-
-	fmt.Println(y)
-
-	r := rawreadField("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", 1}})
-	n := bson.D{{"none", "none"}}
-
-	if reflect.DeepEqual(r, n) {
-		panic("ERROR")
-	}
-	//fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-	//fmt.Println(r[0][1].Value)
-
-	//fmt.Println()
-	//fmt.Printf("TYPE = %T\n\n", r[0][1].Value)
-	//fmt.Printf("TYPE = %T\n\n", r[0][0].Value)
-
-	var these_holdings []holding
-
-	//var temp holding
-
-	switch v := r[0][1].Value.(type) {
-	case bson.A:
-		{
-			// Only works with account holdings
-			these_holdings = mongo_read_bsonA(v)
-		}
-	}
-
-	//value of the trade
-	value := pps * quantity
-
-	// Check if user has the correct holdings
-	for _, holding := range these_holdings {
-		if holding.symbol == stock {
-			//Will rewrite later
-			fmt.Println("TRUE")
-			r := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"cash_balance", value}}, "$inc")
-			i := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", bson.D{{"symbol", holding.symbol}, {"quantity", holding.quantity}, {"pps", holding.pps}}}}, "$pull")
-			if i != "ok" {
-				panic("PUSH ERROR")
-			}
-			f := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", bson.D{{"symbol", stock}, {"quantity", holding.quantity - quantity}, {"pps", pps}}}}, "$push")
-
-			if f != "ok" {
-				panic("PUSH ERROR")
-			}
-			if r != "ok" {
-				panic(r)
-			}
-			//c.IndentedJSON(http.StatusBadRequest, accounts[index])
-			return
-		}
-
-	}
-
-	// User has enough balance, proceed creating order
-	//buy_id := len(orders) + 1
-	//newOrder.Buy_id = buy_id
-	//orders = append(orders, newOrder)
-	//return
-	//c.IndentedJSON(http.StatusOK, newOrder)
-}
-
-func healthcheck(c *gin.Context) {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-	db := c.MustGet("db").(*mongo.Database)
-	err := db.Client().Ping(ctx, readpref.SecondaryPreferred())
-
-	if err == nil {
-		c.String(http.StatusOK, "ok")
-	} else {
-		c.String(http.StatusInternalServerError, "mongo read unavailable")
-		log.Println(err)
-	}
-func addAccount(c *gin.Context) {
-	var newAccount account
-
-	// Call BindJSON to bind the received JSON to newAccount.
-	if err := c.BindJSON(&newAccount); err != nil {
-		return
-	}
-	// Add the new account to the slice.
-	accounts = append(accounts, newAccount)
-	c.IndentedJSON(http.StatusCreated, newAccount)
-}
-
-func addBalance(c *gin.Context) {
-	//id := c.Param("id")
-
-	var addingAmount balanceDif
-	//fmt.Println(addingAmount)
-
-	// Call BindJSON to bind recieved json to newBalance type
-	if err := c.BindJSON(&addingAmount); err != nil {
-		return
-	}
-
-	fmt.Println(addingAmount.ID, addingAmount.Adding)
-
-	for index, i := range accounts {
-		if i.ID == addingAmount.ID {
-			accounts[index].Balance = i.Balance + addingAmount.Adding
-
-			fmt.Println(i.Balance)
-
-			// Change this
-			c.IndentedJSON(http.StatusCreated, accounts)
-		}
 	}
 }
