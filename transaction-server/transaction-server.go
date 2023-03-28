ee234e8b0085943130a0675788fba3224d6c06a8
andreaRA99 <andrea_rmz.alcala@hotmail.com>
2023-03-28 16:36:14 -0700

    addBal validates positive amount

--- transaction-server/transaction-server.go
+++ transaction-server/transaction-server.go
@@ -73,7 +73,8 @@ type order struct {
 }
 
 var quotes = []quote{}
-var orders = []order{}
+var buys = []order{}
+var sells = []order{}
 
 var logfile = []string{} //WILL BE MOVED TO DB
 var transaction_counter = 1
@@ -103,7 +104,7 @@ func main() {
 	router.GET("/users/:id/quote/:stock", Quote)
 	router.POST("/users/buy", buyStock)
 	router.POST("/users/:id/buy/commit", commitBuy)
-	router.POST("/users/:id/sell/:stock/amount/:quantity", sellStock)
+	router.POST("/users/sell", sellStock)
 
 	router.GET("/health", healthcheck)
 
@@ -159,7 +160,7 @@ func getQuotes(c *gin.Context) {
 }
 
 func getOrders(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, orders)
+	c.IndentedJSON(http.StatusOK, buys)
 }
 
 func logAll(c *gin.Context) {
@@ -230,9 +231,14 @@ func addBalance(c *gin.Context) {
 		createAcc(newBalDif.ID)
 	}
 
-	u := updateOne("users", bson.D{{"user_id", newBalDif.ID}}, bson.D{{"cash_balance", newBalDif.Amount}}, "$inc")
-	if u != "ok" {
-		panic(u)
+	if newBalDif.Amount >= 0 {
+		u := updateOne("users", bson.D{{"user_id", newBalDif.ID}}, bson.D{{"cash_balance", newBalDif.Amount}}, "$inc")
+		if u != "ok" {
+			panic(u)
+			c.IndentedJSON(http.StatusOK, u)
+		}
+	} else {
+		c.IndentedJSON(http.StatusForbidden, "Enter valid amount")
 	}
 
 	// LOGGING ACCOUNT CHANGES
@@ -243,8 +249,6 @@ func addBalance(c *gin.Context) {
 	// log_entry = now.String() + " own_server" + t_num + " add " + id + strconv.Itoa(bal)
 	// logfile = append(logfile, log_entry)
 	// log.Println(log_entry)
-
-	c.IndentedJSON(http.StatusOK, u)
 }
 
 func fetchQuote(id string, stock string) quote {
@@ -370,7 +374,7 @@ func buyStock(c *gin.Context) {
 		panic("ERROR")
 	}
 
-	// This would ideally go after checking if accoutn has enough balance
+	// This would ideally go after checking if account has enough balance
 	// Fetching most current price for that stock
 	newOrder.Price = fetchQuote(newOrder.ID, newOrder.Stock).Price
 	newOrder.Qty = int(math.Floor(newOrder.Amount / newOrder.Price))
@@ -381,7 +385,7 @@ func buyStock(c *gin.Context) {
 	case float64:
 		//{
 		if v > newOrder.Amount {
-			orders = append(orders, newOrder)
+			buys = append(buys, newOrder)
 			c.IndentedJSON(http.StatusOK, newOrder)
 			return
 		} else {
@@ -390,7 +394,7 @@ func buyStock(c *gin.Context) {
 	case int64:
 		a := float64(v)
 		if a > newOrder.Amount {
-			orders = append(orders, newOrder)
+			buys = append(buys, newOrder)
 			c.IndentedJSON(http.StatusOK, newOrder)
 			return
 		} else {
@@ -399,7 +403,7 @@ func buyStock(c *gin.Context) {
 	case int32:
 		a := float64(v)
 		if a > newOrder.Amount {
-			orders = append(orders, newOrder)
+			buys = append(buys, newOrder)
 			c.IndentedJSON(http.StatusOK, newOrder)
 			return
 		} else {
@@ -418,12 +422,12 @@ func commitBuy(c *gin.Context) {
 
 	// Getting most recent order that took place within last 60 secs
 	// Queue? Cache?
-	for _, o := range orders {
-		if o.ID == commitOrder.ID && o.Stock == commitOrder.Stock {
+	for _, o := range buys {
+		if o.ID == commitOrder.ID {
 			// change user balance
 			r := updateOne("users", bson.D{{"user_id", commitOrder.ID}}, bson.D{{"cash_balance", -o.Amount}}, "$inc")
 			// add stock to user data
-			i := updateOne("users", bson.D{{"user_id", commitOrder.ID}}, bson.D{{"account_holdings", bson.D{{"symbol", commitOrder.Stock}, {"quantity", o.Qty}}}}, "$push")
+			i := updateOne("users", bson.D{{"user_id", commitOrder.ID}}, bson.D{{"account_holdings", bson.D{{"symbol", commitOrder.Stock}, {"quantity", o.Qty}}}}, "$inc")
 			if i != "ok" {
 				panic("PUSH ERROR")
 			}
@@ -435,96 +439,6 @@ func commitBuy(c *gin.Context) {
 		}
 	}
 
-}
-
-func sellStock(c *gin.Context) {
-	id := c.Param("id")
-	stock := c.Param("stock")
-	quantity, err := strconv.ParseFloat(c.Param("quantity"), 64)
-
-	// SO PROGRAM WILL RUN
-	//pps := getQuoteLocal(stock)
-	pps := 100.00
-
-	if err != nil {
-		panic("ERR")
-	}
-	y := rawreadField("users", bson.D{{"user_id", id}}, bson.D{{"cash_balance", 1}})
-
-	fmt.Println(y)
-
-	r := rawreadField("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", 1}})
-	n := bson.D{{"none", "none"}}
-
-	if reflect.DeepEqual(r, n) {
-		panic("ERROR")
-	}
-	//fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-	//fmt.Println(r[0][1].Value)
-
-	//fmt.Println()
-	//fmt.Printf("TYPE = %T\n\n", r[0][1].Value)
-	//fmt.Printf("TYPE = %T\n\n", r[0][0].Value)
-
-	var these_holdings []holding
-
-	//var temp holding
-
-	switch v := r[0][1].Value.(type) {
-	case bson.A:
-		{
-			// Only works with account holdings
-			these_holdings = mongo_read_bsonA(v)
-		}
-	}
-
-	//value of the trade
-	value := pps * quantity
-
-	// Check if user has the correct holdings
-	for _, holding := range these_holdings {
-		if holding.symbol == stock {
-			//Will rewrite later
-			fmt.Println("TRUE")
-			r := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"cash_balance", value}}, "$inc")
-			i := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", bson.D{{"symbol", holding.symbol}, {"quantity", holding.quantity}, {"pps", holding.pps}}}}, "$pull")
-			if i != "ok" {
-				panic("PUSH ERROR")
-			}
-			f := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", bson.D{{"symbol", stock}, {"quantity", holding.quantity - quantity}, {"pps", pps}}}}, "$push")
-
-			if f != "ok" {
-				panic("PUSH ERROR")
-			}
-			if r != "ok" {
-				panic(r)
-			}
-			//c.IndentedJSON(http.StatusBadRequest, accounts[index])
-			return
-		}
-
-	}
-
-	// User has enough balance, proceed creating order
-	//buy_id := len(orders) + 1
-	//newOrder.Buy_id = buy_id
-	//orders = append(orders, newOrder)
-	//return
-	//c.IndentedJSON(http.StatusOK, newOrder)
-}
-
-func healthcheck(c *gin.Context) {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-	db := c.MustGet("db").(*mongo.Database)
-	err := db.Client().Ping(ctx, readpref.SecondaryPreferred())
-
-	if err == nil {
-		c.String(http.StatusOK, "ok")
-	} else {
-		c.String(http.StatusInternalServerError, "mongo read unavailable")
-		log.Println(err)
-	}
 }
 
 // temp functions to test cli
@@ -543,7 +457,121 @@ func cancelBuy(c *gin.Context) {
 	}
 }
 
+func sellStock(c *gin.Context) {
+	var newOrder order
+
+	// Calling BindJSON to bind the recieved JSON to an order
+	if err := c.BindJSON(&newOrder); err != nil {
+		return
+	}
+
+	// LOGGING USER COMMAND
+	// timestamp-server-transaction-command-username
+	now := time.Now()
+	t_num := strconv.Itoa(transaction_counter) //Is there way to make this global with pointers??
+	transaction_counter += 1
+	var log_entry = now.String() + " own_server " + t_num + " SELL " + newOrder.ID
+	logfile = append(logfile, log_entry)
+
+	y := rawreadField("users", bson.D{{"user_id", newOrder.ID}}, bson.D{{"cash_balance", 1}})
+
+	fmt.Println(y)
+
+	r := rawreadField("users", bson.D{{"user_id", newOrder.ID}}, bson.D{{"account_holdings", 1}})
+	n := bson.D{{"none", "none"}}
+
+	if reflect.DeepEqual(r, n) {
+		panic("ERROR")
+	}
+
+	newOrder.Price = fetchQuote(newOrder.ID, newOrder.Stock).Price
+	newOrder.Qty = int(math.Floor(newOrder.Amount / newOrder.Price))
+	newOrder.Amount = newOrder.Price * float64(newOrder.Qty) // How much user will be charged based on  int Qty of stocks at surr price
+
+	var these_holdings []holding
+
+	switch v := r[0][1].Value.(type) {
+	case bson.A:
+		{
+			// Only works with account holdings
+			these_holdings = mongo_read_bsonA(v)
+		}
+	}
+
+	// Check if user has the correct holdings
+	for _, holding := range these_holdings {
+		if holding.symbol == newOrder.Stock {
+
+			// Check they have enough
+			if holding.quantity >= float64(newOrder.Qty) {
+				sells = append(sells, newOrder)
+				c.IndentedJSON(http.StatusOK, newOrder)
+				return
+			} else {
+				c.IndentedJSON(http.StatusForbidden, "Not enough holdings")
+			}
+
+			//Will rewrite later
+			// fmt.Println("TRUE")
+			// r := updateOne("users", bson.D{{"user_id", newOrder.ID}}, bson.D{{"cash_balance", value}}, "$inc")
+			// i := updateOne("users", bson.D{{"user_id", newOrder.ID}}, bson.D{{"account_holdings", bson.D{{"symbol", holding.symbol}, {"quantity", holding.quantity}, {"pps", holding.pps}}}}, "$pull")
+			// if i != "ok" {
+			// 	panic("PUSH ERROR")
+			// }
+			// f := updateOne("users", bson.D{{"user_id", id}}, bson.D{{"account_holdings", bson.D{{"symbol", stock}, {"quantity", holding.quantity - quantity}, {"pps", pps}}}}, "$push")
+
+			// if f != "ok" {
+			// 	panic("PUSH ERROR")
+			// }
+			// if r != "ok" {
+			// 	panic(r)
+			// }
+			//c.IndentedJSON(http.StatusBadRequest, accounts[index])
+			return
+		} else {
+			c.IndentedJSON(http.StatusForbidden, "No holdings for that stock")
+		}
+
+	}
+
+	// User has enough balance, proceed creating order
+	//buy_id := len(orders) + 1
+	//newOrder.Buy_id = buy_id
+	//orders = append(orders, newOrder)
+	//return
+	//c.IndentedJSON(http.StatusOK, newOrder)
+}
+
 func commitSell(c *gin.Context) {
+	var commitOrder order
+
+	// Calling BindJSON to bind the recieved JSON to new BalDif
+	if err := c.BindJSON(&commitOrder); err != nil {
+		return
+	}
+
+	// Getting most recent order that took place within last 60 secs
+	// Queue? Cache?
+	for _, o := range sells {
+		if o.ID == commitOrder.ID {
+			// change user balance
+			r := updateOne("users", bson.D{{"user_id", commitOrder.ID}}, bson.D{{"cash_balance", +commitOrder.Amount}}, "$inc")
+			// add stock to user data
+			i := updateOne("users", bson.D{{"user_id", commitOrder.ID}}, bson.D{{"account_holdings", bson.D{{"symbol", commitOrder.Stock}, {"quantity", -commitOrder.Qty}}}}, "$inc")
+			if i != "ok" {
+				panic("UPDATE ERROR")
+			}
+			if r != "ok" {
+				panic(r)
+			}
+			//remover order from orders
+			c.IndentedJSON(http.StatusOK, r)
+		}
+	}
+
+}
+
+func cancelSell(c *gin.Context) {
 	// health check code
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
@@ -558,8 +586,7 @@ func commitSell(c *gin.Context) {
 	}
 }
 
-func cancelSell(c *gin.Context) {
-	// health check code
+func healthcheck(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	db := c.MustGet("db").(*mongo.Database)
